c60eba9cf4cf0693a4da1a0c06a6744c3d74484f
d3viant0ne <joshuaw@easymetrics.com>
2021-05-14 20:21:50 -0500

    chore: add material w/ toolbar component

---
+++ src/app/toolbar-component/toolbar-component.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-toolbar-component',
+  templateUrl: './toolbar-component.component.html',
+  styleUrls: ['./toolbar-component.component.scss']
+})
+export class ToolbarComponentComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
